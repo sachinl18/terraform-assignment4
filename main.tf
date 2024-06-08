2bb1f9ddfdc17a4d7fcb50e0f2de3b697731b970
sachinl18 <92222956+sachinl18@users.noreply.github.com>
2024-06-08 13:35:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,3 @@
-terraform {
-  required_version = ">= 0.12"
-}
-
-provider "null" {}
-
-resource "null_resource" "hello" {
-  provisioner "local-exec" {
-    command = "echo Hello World"
-  }
-}
-
-output "hello_message" {
-  value = "Hello World"
+output "hello_world" {
+    value = "Hello, World!"
 }
